357d5974df9834ab5f635b42e0fc5c17cc7441ee
Evgeny Dunaev <evgeny.dunaev@capgemini.com>
2021-08-25 14:09:41 +0200

    changed structure

---
+++ src/main/java/myfinanceapp/Main.java
@@ -0,0 +1,10 @@
+package myfinanceapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/test/java/myfinanceapp/MainTest.java
@@ -0,0 +1,7 @@
+package myfinanceapp;
+
+public class MainTest {
+
+
+
+}

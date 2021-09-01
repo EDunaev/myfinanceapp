ab27eb62a03a2a7b45e02f4056fad71cf71b07fa
Evgeny Dunaev <evgeny.dunaev@capgemini.com>
2021-09-01 10:31:38 +0200

    mapper

---
+++ src/main/java/com/dunaevi/mapper/FamilyIncomeMapper.java
@@ -0,0 +1,14 @@
+package com.dunaevi.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.dunaevi.controller.to.FamilyIncomeTo;
+import com.dunaevi.entity.FamilyIncome;
+
+@Mapper(componentModel = "spring")
+public interface FamilyIncomeMapper {
+
+    
+    FamilyIncomeTo mapEntityToTo(
+            FamilyIncome source);
+}
